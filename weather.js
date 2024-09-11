c2c9e0fe2f68fc93abd221593c491ec812049738
udaybscitstudent <162083596+udaybscitstudent@users.noreply.github.com>
2024-09-11 18:59:42 +0530

    weather for casting

---
+++ weather.js
@@ -0,0 +1,42 @@
+
+        const apiKey = "206641a86f149e329b1a3251be536964";
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+        
+        // const searchBox = document.querySelector(".search input");
+        const searchBox=  document.querySelector(".search input");
+        const searchBtn = document.querySelector(".search button");
+        const weatherIcon = document.querySelector(".whether-icon");
+
+        console.log(searchBox);
+        async function checkWeather(city){
+            const response = await fetch(apiUrl+ city +`&appid=${apiKey}`);
+            var data = await response.json();
+            console.log(data);
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+            if(data.weather[0].main == "Clouds"){
+                weatherIcon.src= "images/clouds.png";
+            }
+            else if(data.weather[0].main == "Clear"){
+                weatherIcon.src= "images/clear.png";
+            }
+            else if(data.weather[0].main == "Rain"){
+                weatherIcon.src= "images/rain.png";
+            }
+            else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src= "images/drizzle.png";
+            }
+            if(data.weather[0].main == "Mist"){
+                weatherIcon.src= "images/mist.png";
+            }
+
+
+
+        }
+        searchBtn.addEventListener("click",() => {
+            checkWeather(searchBox.value);
+        })
+        checkWeather("patna");
